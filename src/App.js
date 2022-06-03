b5c56e49f54ef113226afca7ff7a2cdf5e0e90ca
yoo-hyeon-sik <hs.yoo@wyhil.com>
2022-06-03 16:31:29 +0900

    # basename={process.env.PUBLIC_URL}

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Detail from "./routes/Detail";
 
 
 function App() {
-    return <Router>
+    return <Router basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/movie/:id" element={<Detail />} />
             <Route path="/" element={<Home />} />
