4a8e2656474d55629573b4de372ae08802a897a7
yoo-hyeon-sik <hs.yoo@wyhil.com>
2022-06-03 16:12:49 +0900

    # fileupload

---
+++ src/routes/Detail.js
@@ -0,0 +1,17 @@
+import {useParams} from "react-router-dom";
+import {useEffect} from "react";
+//https://yts.mx/api/v2/movie_details.json?movie_id=
+function Detail(){
+    const {id} = useParams();
+    const getMovice = async()=>{
+        const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json()
+        console.log(json)
+    }
+    useEffect((async)=>{
+        getMovice();
+    },[])
+
+    return <h1>Detail{id}</h1>
+}
+
+export default Detail
---
+++ src/routes/Home.js
@@ -0,0 +1,47 @@
+
+import Movie from "../components/Movie";
+import {useState, useEffect} from "react";
+
+function Home(){
+
+    const [loading, setLoading] = useState(true)
+    const [movies, setMovies] = useState([])
+    const getMovies = async()=>{
+        const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+
+    }
+
+    useEffect(()=>{
+        getMovies()
+
+    },[])
+
+
+    return (
+        <div className="App">
+            {loading? <h1>Loading</h1> :
+                <div>
+                    {
+                        movies.map((movie, index) => {
+                            return <Movie
+                                key={<movie className="id"></movie>}
+                                id={movie.id}
+                                coverImage={movie.medium_cover_image}
+                                title={movie.title}
+                                summary={movie.summary}
+                                genres={movie.genres}
+                            />
+                        })
+                    }
+                </div>
+            }
+
+        </div>
+    );
+
+}
+
+export default Home
